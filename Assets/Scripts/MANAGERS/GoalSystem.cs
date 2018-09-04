4e942e3bf8f6770a59afc2e1e67b84001c64c973
sangre5 <pjuradosolis@gmail.com>
2018-09-04 15:09:44 +0200

    Implemented a goal system trigger

--- Assets/Scripts/MANAGERS/GoalSystem.cs
+++ Assets/Scripts/MANAGERS/GoalSystem.cs
@@ -1,16 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine.SceneManagement;
 
-public class GoalSystem : MonoBehaviour {
+public class GoalSystem : ComponentSystem {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private struct Goal
+    {
+        public GoalComponent gc;
+    }
+
+    private struct Data
+    {
+        public ComponentArray<PlayerInput> InputComponents;
+    }
+
+    [Inject] private Data _data;
+    protected override void OnUpdate()
+    {
+        var GoalEntity = GetEntities<Goal>();
+
+
+        if (GoalEntity.Length > 0)
+        {
+            var distance = math.distance(GoalEntity[0].gc.transform.position.x, _data.InputComponents[0].transform.position.x);
+
+
+            //
+            if (distance < 1)
+            {
+                SceneManager.LoadScene("WinYacht");
+
+            }
+        }
+    }
 }
