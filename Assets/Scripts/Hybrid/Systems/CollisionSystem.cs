f5c6009fa7cdd35ae1ad8f31d2fded69f87bcadd
SupremeNewb <royvanstralen@hotmail.com>
2018-09-13 08:53:00 +0200

    added collision system

---
+++ Assets/Scripts/Hybrid/Systems/CollisionSystem.cs
@@ -0,0 +1,26 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace Assets.Scripts.Hybrid.Systems
+{
+    class CollisionSystem : ComponentSystem
+    {
+        public struct PlayerGroup
+        {
+            public Transform Transform;
+            public PlayerInput PlayerInput;
+            public Speed Speed;
+            public Rigidbody2D rb;
+            public Jump Jump;
+        }
+
+        protected override void OnUpdate()
+        {
+            foreach (var entity in GetEntities<PlayerGroup>())
+            {
+                entity.Jump.IsGrounded = Physics2D.OverlapBox(new Vector2(entity.Jump.Feet.position.x, entity.Jump.Feet.position.y), new Vector2(entity.Jump.BoxWidth, entity.Jump.BoxHeight), 360.0f, entity.Jump.WhatIsGround);
+                
+            }
+        }
+    }
+}
