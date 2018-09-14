2564817609cf223d7354ef747f15d5dab7485368
Xiang-jong Hu <xiang_jonghu@hotmail.com>
2018-09-14 15:29:44 +0200

    Changes to the scenes for cat, feet, menus. Cat cannot move

---
+++ Assets/Scripts/Hybrid/Systems/MovementSystem.cs
@@ -0,0 +1,34 @@
+﻿using Assets.Scripts.Hybrid.Components;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Assets.Scripts.Hybrid.Systems
+{
+    public class MovementSystem : ComponentSystem
+    {
+
+        public struct PlayerGroup
+        {
+            public Transform Transform;
+            public PlayerInput PlayerInput;
+            public Speed Speed;
+            public Rigidbody2D rb;
+            public SpriteRenderer sr;
+            public Animator Anim;
+        }
+
+        protected override void OnUpdate()
+        {
+            float dt = Time.deltaTime;
+
+            foreach (var entity in GetEntities<PlayerGroup>())
+            {
+                Vector2 position = entity.Transform.position;
+
+                position.x += entity.Speed.XSpeed * entity.PlayerInput.Horizontal * dt;
+
+                entity.Transform.position = position;
+            }
+        }
+    }
+}
